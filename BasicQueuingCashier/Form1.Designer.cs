b6ab449652c1d62af8a10d007ecb71126eb1e3af
GBaltazar20 <g.baltazzar0022@gmail.com>
2025-08-20 18:40:23 +0800

    Add project files.

---
+++ BasicQueuingCashier/Form1.Designer.cs
@@ -0,0 +1,88 @@
+﻿namespace BasicQueuingCashier
+{
+    partial class QueuingForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnCashier = new System.Windows.Forms.Button();
+            this.lblQueue = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // btnCashier
+            // 
+            this.btnCashier.Location = new System.Drawing.Point(66, 127);
+            this.btnCashier.Name = "btnCashier";
+            this.btnCashier.Size = new System.Drawing.Size(75, 69);
+            this.btnCashier.TabIndex = 0;
+            this.btnCashier.Text = "Cashier";
+            this.btnCashier.UseVisualStyleBackColor = true;
+            // 
+            // lblQueue
+            // 
+            this.lblQueue.AutoSize = true;
+            this.lblQueue.Font = new System.Drawing.Font("Microsoft Sans Serif", 27.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblQueue.Location = new System.Drawing.Point(147, 154);
+            this.lblQueue.Name = "lblQueue";
+            this.lblQueue.Size = new System.Drawing.Size(189, 42);
+            this.lblQueue.TabIndex = 1;
+            this.lblQueue.Text = "P - 10007";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(177, 127);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(133, 20);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "Position in Queue";
+            this.label2.Click += new System.EventHandler(this.label2_Click);
+            // 
+            // QueuingForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(478, 450);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.lblQueue);
+            this.Controls.Add(this.btnCashier);
+            this.Name = "QueuingForm";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnCashier;
+        private System.Windows.Forms.Label lblQueue;
+        private System.Windows.Forms.Label label2;
+    }
+}
+
